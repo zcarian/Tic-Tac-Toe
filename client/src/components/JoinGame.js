0166b8293eab8fa2b877493aba3bbea43428fd94
zcarian <mar.jed.00@gmail.com>
2023-12-28 06:25:31 +0100

    adding CustomInput

--- client/src/components/JoinGame.js
+++ client/src/components/JoinGame.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useChatContext, Channel } from "stream-chat-react";
 import Game from "./Game";
+import CustomInput from "./CustomInput";
 
 export default function JoinGame() {
   const [rivalUsername, setRivalUsername] = useState("");
@@ -26,7 +27,7 @@ export default function JoinGame() {
   return (
     <>
       {channel ? (
-        <Channel channel={channel}>
+        <Channel channel={channel} Input={CustomInput}>
           <Game channel={channel} />
         </Channel>
       ) : (
